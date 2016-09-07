ac8d4157118e7d67b57b8cad3663e06aa1e69257
Juha Sonck <juha.sonck@pointcollege.fi>
2016-09-07 14:48:50 +0300

    Sovelluksen tyhjä alkuversio

---
+++ AlyTaloMob1/AlyTaloMob1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlyTaloMob1.Startup))]
+namespace AlyTaloMob1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
